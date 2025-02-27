ce84e5464ff6f40e436e1ad36c3bc85567f68b15
Lukasz Rybarz <lukasz.ry@modulesgarden.com>
2025-02-27 04:18:23 +0100

    Getting rid of hack language

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-  include 'includes/header.php';
+  include 'includes/header.html';
 ?>
 
 <main class="container mx-auto p-6">
   <?php
-    include 'includes/about.php';
-    include 'includes/skills.php';
-    include 'includes/experience.php';
-    include 'includes/contact.php';
+    include 'includes/about.html';
+    include 'includes/skills.html';
+    include 'includes/experience.html';
+    include 'includes/contact.html';
   ?>
 </main>
 
 <?php
-  include 'includes/footer.php';
+  include 'includes/footer.html';
 ?>
