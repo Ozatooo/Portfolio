c19942fc75ab9ce4637d5b9c647323773e165a29
Lukasz Rybarz <lukasz.ry@modulesgarden.com>
2025-02-27 04:55:32 +0100

    Fix mobile experience view and add smooth scroll

--- script.js
+++ script.js
@@ -67,4 +67,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.getElementById("year").textContent = new Date().getFullYear();
